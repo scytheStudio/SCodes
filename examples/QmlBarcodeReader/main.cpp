e32a0a47d556f6fb9ce9c530faba45bd13fe1ca8
Lukas Kosinski <lukasz@scythe-studio.com>
2020-11-11 12:02:28 +0100

    #1 ifdef to register BarcodeFilter for Qt before 5.15.0 in QmlBarcodeReader main.cpp added

--- examples/QmlBarcodeReader/main.cpp
+++ examples/QmlBarcodeReader/main.cpp
@@ -8,7 +8,9 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     // If you use Qt version older than 5.15 you need to manually register BarcodeFilter
-    //qmlRegisterType<BarcodeFilter>("com.scythestudio.sbarcodereader", 1, 0, "BarcodeFilter");
+#if (QT_VERSION < QT_VERSION_CHECK(5, 15, 0))
+        qmlRegisterType<BarcodeFilter>("com.scythestudio.sbarcodereader", 1, 0, "BarcodeFilter");
+#endif
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/qml/ScannerPage.qml")));
